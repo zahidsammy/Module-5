2ea127d48265f68e64ca29d46ea070fac4049efd
zahidsammy <120346801+zahidsammy@users.noreply.github.com>
2023-11-23 08:31:36 +1300

    Add files via upload

---
+++ Models/index.js
@@ -0,0 +1,21 @@
+// 'use strict'
+
+// const Friends = require('./friends') //require the model
+// //const Users = require('./users') //require the model
+// //const Services = require('./services') //require the model
+
+// async function init() {
+//     //await Users.sync();
+//     await Friends.sync();
+//     //await Services.sync();
+  
+    
+//     //sync the model
+// };
+
+// init();
+// module.exports = {
+//     Friends, //export the model
+//     //Users, //export the model
+//     //Services //export the model
+// };
